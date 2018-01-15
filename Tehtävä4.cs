0a0ed9ff91f87411159368244892c5ed787575f2
RajuJanne <moby@insomnia.fi>
2018-01-15 20:05:53 +0200

    Viikko 1 tehtävät + luennot
    Harjoituksen 1 tehtävät 1-5
    Viikon 3 luentojen koodaukset (Program.cs ja Program2.cs)

---
+++ Tehtävä4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Tehtava_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int koko;
+            Write("Piirrän sinulle kolmion. Miten suuren haluat? ");
+            koko = int.Parse(System.Console.ReadLine());
+            WriteLine("");
+            //System.Threading.Thread.Sleep(10000);
+            TulostaKolmio(koko);
+            ReadLine();
+        }
+
+        static void TulostaTyhjaa(int maara) {
+            for (int i = 0; i < maara; i++ ) {
+              Write(" ");
+            }
+        }
+
+        static void TulostaTahtia(int lkm) {
+            for (int i = 0; i < lkm; i++) {
+              Write("*");
+            }
+            WriteLine("");
+        }
+        static void TulostaKolmio(int koko) {
+           int maara = 0;
+           int lkm = koko;
+
+             for (int i = 0; i < koko; i++) {
+              TulostaTyhjaa(maara);
+              TulostaTahtia(lkm);
+              maara++;
+              lkm--;
+             }
+        }
+
+    }
+}
