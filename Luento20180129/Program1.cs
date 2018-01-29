c83ca6d312debe3b1f407a1441ec780d394c3531
RajuJanne <moby@insomnia.fi>
2018-01-29 11:23:00 +0200

    Ahah, ohoh!

--- Luento20180129/Program1.cs
+++ Luento20180129/Program1.cs
@@ -33,8 +33,24 @@ namespace L0129
             Write("Anna desimaaliluku [-100,100]: ");
             syote = ReadLine();
             //bool success = Double.TryParse(syote, out luku);
-            luku = double.Parse(syote);
-            WriteLine("syötit luvun " + luku);
+            try
+            {
+                luku = double.Parse(syote);
+                if (luku < -100 || luku > 100)
+                {
+                    throw new ApplicationException("Luku ei ole parametrien rajoissa.");
+                }
+
+                WriteLine("syötit luvun " + luku);
+            }
+            catch (FormatException)
+            {
+                WriteLine("Annoit paskan syötteen vammane.");
+            }
+            catch (Exception e)
+            {
+                WriteLine(e.Message);
+            }
         }
     }
 }
