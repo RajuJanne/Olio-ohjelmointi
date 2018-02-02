a86382640905486550e5fde703d7ce5e7227e5a0
RajuJanne <moby@insomnia.fi>
2018-02-02 19:25:07 +0200

    H2 etenee

--- Harjoitus 2/Harjoitus2_tehtävä2.cs
+++ Harjoitus 2/Harjoitus2_tehtävä2.cs
@@ -1,16 +1,56 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using static System.Console;
 
 //Janne Rajuvaara - 2018
 //NTK17SP
 
-namespace H2_Tehtava_2
+namespace H2T2
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
 
+        static void Main(string[] args)
+        {
+            long pitka;
+            Write("Anna pitkä luku: ");
+            pitka = long.Parse(ReadLine());
+            TulostaOnnistuuko(pitka);
+            ReadLine();
+        }
+
+        static void TulostaOnnistuuko(long pitka)
+        {
+            int tulos;
+            if (LongToInt(pitka, out tulos))
+            {
+                WriteLine($"{pitka} (long) --> {tulos} (int)");
+            }
+            else
+            {
+                WriteLine($"Lukua {pitka} ei voi muuttaa int-luvuksi");
+            }
+        }
+
+        static bool LongToInt(long pitka, out int tulos)
+        {
+            bool onnistuu;
+            int viite;
+            if (pitka > int.MaxValue || pitka < int.MinValue)
+            {
+                onnistuu = false;
+                viite = 0;
+            }
+            else
+            {
+                viite = (int)pitka;
+                onnistuu = true;
+            }
+            tulos = viite;
+            return onnistuu;
+        }
     }
-  }
 }
--- Harjoitus 2/Harjoitus2_tehtävä3.cs
+++ Harjoitus 2/Harjoitus2_tehtävä3.cs
@@ -1,16 +1,68 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using static System.Console;
 
 //Janne Rajuvaara - 2018
 //NTK17SP
 
-namespace H2_Tehtava_3
+namespace H2T3
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
+            Write("Syötä teksti: ");
+            string merkkijono = ReadLine();
+            if (OnkoPalindromi(merkkijono))
+            {
+                Write("On palindromi.");
+            }
+            else
+            {
+                Write("Ei ole palindromi.");
+            }
+            ReadLine();
+        }
 
+         static bool OnkoPalindromi(string merkkijono)
+        {
+            if (PoistaValimerkit(merkkijono) == Kaanna(merkkijono))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static string Kaanna(string merkkijono)
+        {
+            string paluu = "";
+            merkkijono = PoistaValimerkit(merkkijono);
+            for (int i = merkkijono.Length - 1; i >= 0; i--)
+            {
+                paluu = paluu + merkkijono[i];
+            }
+            return paluu;
+        }
+
+        static string PoistaValimerkit(string merkkijono)
+        {
+            string paluu = "";
+            char[] skipattavat = {' ', '.', ':', ';', ',', '!', '?'};
+
+            for (int i = 0; i < merkkijono.Length; i++)
+            {
+                if (!skipattavat.Contains(merkkijono[i]))
+                {
+                    paluu = paluu + merkkijono[i];
+                }
+            }
+            return paluu;
+        }
     }
-  }
 }
