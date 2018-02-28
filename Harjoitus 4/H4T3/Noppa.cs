25d4048fd04379778b1856deead3104413a5e0d5
RajuJanne <moby@insomnia.fi>
2018-02-28 10:36:30 +0200

    Luennolla oli jälleen jännää.

---
+++ Harjoitus 4/H4T3/Noppa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+//Janne Rajuvaara
+//NTK17SP 2018
+
+namespace H4T3
+{
+    class Noppa
+    {
+        private int _lukema, _heittoLkm;
+        private double _kumulatiivinen;
+
+        public Noppa()
+        {
+            _lukema = -1;
+            _heittoLkm = 0;
+            _kumulatiivinen = 0;
+        }
+
+        public void Heita()
+        {
+            Random r = new Random();
+            _lukema = r.Next(1,7);
+            _heittoLkm++;
+            _kumulatiivinen += _lukema;
+        }
+
+        public int NaytaLukema()
+        {
+            return _lukema;
+        }
+
+        public int NaytaHeittojenMaara()
+        {
+            return _heittoLkm;
+        }
+
+        public double Kumulatiivinen()
+        {
+            return _kumulatiivinen;
+        }
+    }
+}
---
+++ Harjoitus 4/H4T3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+
+//Janne Rajuvaara
+//NTK17SP 2018
+
+namespace H4T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Noppa noppa1 = new Noppa();
+            noppa1.Heita();
+            WriteLine("Nopan silmäluku: " + noppa1.NaytaLukema());
+
+            do
+            {
+                Write("Heitetäänkö vielä (kyllä/ei)? ");
+                string syote = ReadLine();
+                try
+                {
+                    if (syote == "kyllä")
+                    {
+                        noppa1.Heita();
+                        WriteLine("Nopan silmäluku: " + noppa1.NaytaLukema());
+                    }
+                    else if (syote == "ei")
+                    {
+                        WriteLine("Heittojen määrä on " + noppa1.NaytaHeittojenMaara());
+                        WriteLine("Heittojen keskiarvo on " + noppa1.Kumulatiivinen() / noppa1.NaytaHeittojenMaara());
+                        break;
+                    }
+                    else
+                    {
+                        throw new Exception("Epäkelpo syöte");
+                    }
+                }
+                catch (Exception e)
+                {
+                    WriteLine(e.Message);
+                }
+
+            } while (true);
+            ReadLine();
+
+        }
+
+    }
+}
